ff67cbed13e29751769a2fb17fba508c9d44791d
09jkitchen <09jkitchen@live.com>
2017-04-30 02:31:56 -0600

    Progress bar & wait to play
    Added a progress bar b/c the IE loading bar is garbage. Added waiting to
    play being dependent on onCanPlayThrough to allow a little buffer to
    build up. No longer use a USB drive, as the SD card is faster.

--- index.php
+++ index.php
@@ -17,16 +17,18 @@ span {
   <source src="" type="audio/mpeg">
 </audio>
 </td><td>
-<button type="button" onclick="randSong()">Shuffle</button>
+<button type="button" id="reload" onclick="reload()">Reload</button>
+<button type="button" id="shuffle" onclick="randSong()">Shuffle</button>
 </td></tr></table>
-Now Playing:<span id="songTitle" style="color:black">SONG TITLE</span>
+<progress id="myProgress" value="0" max="100"></progress> <br>
+Now Playing:<span id="songTitle" style="color:black">SONG TITLE</span>   <span id="buffer" style="color:black">Buffering</span>
 <br>
 <div style="height:700px;overflow:auto">
 <table border=1>
 <?php
 
-$mp3s = glob("usbdrv/Music/*.mp3");
-$m4as = glob("usbdrv/Music/*.m4a");
+$mp3s = glob("Music/*.mp3");
+$m4as = glob("Music/*.m4a");
 $songs = array_merge($mp3s, $m4as);
 sort($songs);
 $nums = sizeof($songs);
@@ -36,29 +38,58 @@ foreach ($songs as $song) {
    $count += 1;
    $songO = $song;
    $song = addslashes($song);
-// echo '<tr><td><span id="'.$count.'" onclick="loadSong(\''.$song.'\')">'.$song.'</span></td></tr>';
-   echo '<tr onclick="loadSong('.$count.',\''.$song.'\')"><td><span id="'.$count.'">'.$songO.'</span></td></tr>'; 
+   echo '<tr onclick="loadSong('.$count.',\''.$song.'\')"><td><span id="'.$count.'">'.$songO.'</span></td></tr>';
 }
 ?>
 </table>
 </div>
 <script>
-  var aud = document.getElementById("myAudio"); 
-  function loadSong(id, name) {
-    aud.src = name;
-    aud.load();
-    aud.play();
-    document.getElementById("songTitle").innerText = name;
-    document.getElementById(id).style.color = "purple";
+  var aud = document.getElementById("myAudio");
+  var prog = document.getElementById("myProgress");
+  aud.addEventListener("canplaythrough", bufferThrough)
+  aud.addEventListener("progress",progEvent);
+
+  function bufferThrough()
+  {
+	  document.getElementById("buffer").innerText = "Should Buffer";
+	  aud.play();
+  }
+
+  function progEvent()
+  {
+	  var duration = aud.duration;
+	  var z = aud.buffered.end(aud.buffered.length-1);
+	  var loaded = ((z / duration)*100).toFixed(0);
+	  prog.value = loaded;
+	  if (z == duration)
+	  {
+		  document.getElementById("buffer").innerText = "Finished Buffering";
+	  }
+  }
+
+  function loadSong(id, name)
+  {
+	  aud.src = name;
+	  aud.load();
+	  document.getElementById("songTitle").innerText = name;
+	  document.getElementById(id).style.color = "purple";
+	  document.getElementById("buffer").innerText="Buffering";
   }
   function randSong () {
-    var max = document.getElementById("numSongs").innerText;
-    var rndm = Math.floor(Math.random() * max) + 1;
-    aud.src = document.getElementById(rndm).innerText;
+	  var max = document.getElementById("numSongs").innerText;
+	  var rndm = Math.floor(Math.random() * max) + 1;
+	  aud.src = document.getElementById(rndm).innerText;
+	  aud.load();
+	  document.getElementById("songTitle").innerText = document.getElementById(rndm).innerText;
+	  document.getElementById(rndm).style.color = "purple";
+	  document.getElementById("buffer").innerText = "Buffering";
+  }
+
+  function reload() {
+    var currSrc = aud.currentSrc;
+    aud.src = currSrc;
     aud.load();
-    aud.play();
-    document.getElementById("songTitle").innerText = document.getElementById(rndm).innerText;
-    document.getElementById(rndm).style.color = "purple";
+    document.getElementById("buffer").innerText = "Buffering";
   }
 </script>
 
